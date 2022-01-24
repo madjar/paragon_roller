12a8ab77b7707855db3c9b5e5f064135f24caedc
Georges Dubus <georges.dubus@soundcloud.com>
2022-01-24 18:38:33 +0100

    Dice parser: handle flat modifier, and validate deice size

--- lib/paragon_roller/engine/dice_pool.ex
+++ lib/paragon_roller/engine/dice_pool.ex
@@ -5,15 +5,39 @@ defmodule ParagonRoller.Engine.DicePool do
   defstruct [:dice, :flat]
 
   @type t :: %DicePool{
-          dice: [{count :: integer(), faces :: integer()}],
+          dice: %{(count :: integer()) => faces :: integer()},
           flat: integer() | nil
         }
 
+  @known_dice [4, 6, 8, 10, 12, 20]
+
+  @spec parse(binary) ::
+          {:error, {:token_parsing_failed, any} | {:tokenizing_failed, any}}
+          | {:ok, ParagonRoller.Engine.DicePool.t()}
   def parse(str) do
+    with {:ok, list} <- parse_raw(str) do
+      {flat, dice_map} =
+        list
+        |> Enum.reduce(%{}, fn {count, dice}, acc ->
+          Map.update(acc, dice, count, &(&1 + count))
+        end)
+        |> Map.pop(1)
+
+      {validated_dice_map, bad_map} = Map.split(dice_map, @known_dice)
+
+      if bad_map == %{} do
+        {:ok, %DicePool{dice: validated_dice_map, flat: flat}}
+      else
+        {:error, :unknown_dice_size, bad_map |> Map.keys() |> Enum.map(&Integer.to_string/1)}
+      end
+    end
+  end
+
+  defp parse_raw(str) do
     with charlist <- String.to_charlist(str),
          {:ok, tokens, _} <- :dice_lexer.string(charlist),
          {:ok, list} <- :dice_parser.parse(tokens) do
-      {:ok, %DicePool{dice: list}}
+      {:ok, list}
     else
       {:error, {1, :dice_lexer, reason}, 1} ->
         {:error, {:tokenizing_failed, reason}}
--- src/dice_parser.yrl
+++ src/dice_parser.yrl
@@ -15,5 +15,6 @@ Rootsymbol expr.
 expr -> roll: ['$1'].
 expr -> roll separator expr : ['$1'| '$3'].
 roll -> integer d integer : {'$1', '$3'}.
+roll -> integer : {'$1', 1}.
 
 integer -> int : element(3, '$1').
